622b3a65e087072bd7d2c0eda4480e1f66b5d73b
aprybysh <alprybysh@gmail.com>
2018-07-17 12:21:45 -0700

    Added test retry Rule

--- app/src/androidTest/java/com/example/android/test/tests/RegressionTests.java
+++ app/src/androidTest/java/com/example/android/test/tests/RegressionTests.java
@@ -1,13 +1,18 @@
 package com.example.android.test.tests;
 import org.junit.FixMethodOrder;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 
 import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.UiObjectNotFoundException;
 
 import com.example.android.test.steps.ChannelTabSteps;
 import com.example.android.test.steps.MainActivitySteps;
+import com.example.android.test.helpers.Retry;
+import com.example.android.test.helpers.RetryRule;
 import com.example.android.test.steps.SystemStep;
 import com.example.android.test.steps.VideoTabSteps;
 
@@ -16,6 +21,7 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+@RunWith(AndroidJUnit4.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class RegressionTests {
     static UiDevice mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
@@ -33,15 +39,21 @@ public class RegressionTests {
         systemStep.startApp();
         systemStep.dismissInitialDialog();
     }
+    //Set retry count argument
+    @Rule
+    public RetryRule retryRule = new RetryRule(3);
 
     @Test
-    public void Test1openVideo() {
+    @Retry
+    public void test1OpenVideo() {
         steps.openVideo();
         Assert.assertTrue(videoTabSteps.isVideoOpen());
     }
 
     @Test
-    public void Test2checkChannel() {
+    @Retry
+    public void test2CheckChannel() {
+        steps.openVideo();
         videoTabSteps.openChannel();
         Assert.assertTrue(channelTabSteps.channelIsOPen());
     }
--- app/src/androidTest/java/com/example/android/test/tests/SmokeTests.java
+++ app/src/androidTest/java/com/example/android/test/tests/SmokeTests.java
@@ -1,26 +1,29 @@
 package com.example.android.test.tests;
 
 import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.UiObjectNotFoundException;
 
+import com.example.android.test.helpers.Retry;
+import com.example.android.test.helpers.RetryRule;
 import com.example.android.test.steps.TrendingSteps;
 import com.example.android.test.steps.MainActivitySteps;
 import com.example.android.test.steps.SystemStep;
 
-import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 
 import java.io.IOException;
 
 import static org.junit.Assert.assertThat;
-
+@RunWith(AndroidJUnit4.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SmokeTests {
 
@@ -41,7 +44,12 @@ public class SmokeTests {
 
     }
 
+    //Set retry count argument
+    @Rule
+    public RetryRule retryRule = new RetryRule(3);
+
     @Test
+    @Retry
     public void test1PlayVideo() {
         steps.playFirstVideo();
 
@@ -71,6 +79,7 @@ public class SmokeTests {
     }
 
     @Test
+    @Retry
     public void test2OpenTrending() {
         steps.openTrending();
         Assert.assertTrue(trendingSteps.isTrendingOpen());
